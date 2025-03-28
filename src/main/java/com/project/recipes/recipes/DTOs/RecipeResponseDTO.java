1707c1e26c4ff1791569c00565f7bac1a1082cde
RyanVictorDev <vryan8294@gmail.com>
2025-03-28 20:44:44 -0300

    Dockerizing application

--- src/main/java/com/project/recipes/recipes/DTOs/RecipeResponseDTO.java
+++ src/main/java/com/project/recipes/recipes/DTOs/RecipeResponseDTO.java
@@ -2,7 +2,6 @@ package com.project.recipes.recipes.DTOs;
 
 import com.project.recipes.recipes.models.RecipeTagEnum;
 import com.project.recipes.users.DTOs.UserResponseDTO;
-import com.project.recipes.users.models.UserModel;
 import lombok.Builder;
 
 @Builder
